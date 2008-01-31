e78e1d3be99cf47c0e80102f455d8c99e324aaa4
chris4136 <chris4136@ca06981c-f144-0410-9d5b-9b1bebc65a9f>
2008-01-31 21:04:51 +0000

    Working on passing all needed variables to updateworkout.php.  Still a work in progress.
    -cs

--- showworkout.php
+++ showworkout.php
@@ -30,7 +30,7 @@ $showWorkout = new database_mysql();
 $showWorkout->connect();
 
 ## MySQL SQL Data Pull ##
-$sql = 'SELECT `UsrWghtUsrID` , `UsrWghtRoutID` , `UsrWghtExerID` , `UsrWghtSessID` , `UsrWghtDate` , `MuscleGrpName` , `UsrExerName` , `UsrSetsQTY` , `UsrWghtSet1` , `UsrWghtSet2` , `UsrWghtSet3` , `UsrWghtSet4` , `UsrWghtSet5` , `UsrWghtSet6` , `UsrWghtSet7` , `UsrWghtSet8` , `UsrWghtSet9` , `UsrWghtSet10` , `UsrExerMuscleGrp` FROM `usrweighttbl` , `usrsetstbl` , `usrexercisetbl` , `musclegrouptbl` WHERE `usrwghtsetsid` = `usrsetsid` AND `usrwghtexerid` = `usrexerid` AND `usrexermusclegrp` = `musclegrpid` AND `usrwghtdate` = "2008-01-11" ORDER BY MuscleGrpName LIMIT 0, 30 ';
+$sql = 'SELECT `UsrWghtID`, `UsrWghtUsrID` , `UsrWghtRoutID` , `UsrWghtExerID` , `UsrWghtSessID` , `UsrWghtDate` , `MuscleGrpName` , `UsrExerName` , `UsrSetsQTY` , `UsrWghtSet1` , `UsrWghtSet2` , `UsrWghtSet3` , `UsrWghtSet4` , `UsrWghtSet5` , `UsrWghtSet6` , `UsrWghtSet7` , `UsrWghtSet8` , `UsrWghtSet9` , `UsrWghtSet10` , `UsrExerMuscleGrp` FROM `usrweighttbl` , `usrsetstbl` , `usrexercisetbl` , `musclegrouptbl` WHERE `usrwghtsetsid` = `usrsetsid` AND `usrwghtexerid` = `usrexerid` AND `usrexermusclegrp` = `musclegrpid` AND `usrwghtdate` = "2008-01-11" ORDER BY MuscleGrpName LIMIT 0, 30 ';
 
 ## Execute Query ##
 $showWorkout->query($sql);
@@ -41,17 +41,17 @@ $num = $showWorkout->num_rows();
 $i = 0;
 while($row = $showWorkout->fetch_array()) {
      $UsrWghtID[$i] = $row['UsrWghtID'];
-		 $UsrWghtRoutID[$i] = $row['UsrWghtRoutID'];
-		 $UsrWghtExerID[$i] = $row['UsrWghtExerID'];
-		 $UsrWghtSessID[$i] = $row['UsrWghtSessID'];
-		 $UsrWghtDate[$i] = $row['UsrWghtDate'];
-		 $MuscleGrpName[$i] = $row['MuscleGrpName'];
-		 $UsrExerName[$i] = $row['UsrExerName'];
-		 $UsrSetsQTY[$i] = $row['UsrSetsQTY'];
-		 $UsrWghtSet[$i] = array($row['UsrWghtSet1'], $row['UsrWghtSet2'], $row['UsrWghtSet3'], 
-		 $row['UsrWghtSet4'], $row['UsrWghtSet5'], $row['UsrWghtSet6'], $row['UsrWghtSet7'], 
-		 $row['UsrWghtSet8'], $row['UsrWghtSet9'], $row['UsrWghtSet10']);
-		 $i++;
+     $UsrWghtRoutID[$i] = $row['UsrWghtRoutID'];
+     $UsrWghtExerID[$i] = $row['UsrWghtExerID'];
+     $UsrWghtSessID[$i] = $row['UsrWghtSessID'];
+     $UsrWghtDate[$i] = $row['UsrWghtDate'];
+     $MuscleGrpName[$i] = $row['MuscleGrpName'];
+     $UsrExerName[$i] = $row['UsrExerName'];
+     $UsrSetsQTY[$i] = $row['UsrSetsQTY'];
+     $UsrWghtSet[$i] = array($row['UsrWghtSet1'], $row['UsrWghtSet2'], $row['UsrWghtSet3'], 
+     $row['UsrWghtSet4'], $row['UsrWghtSet5'], $row['UsrWghtSet6'], $row['UsrWghtSet7'], 
+     $row['UsrWghtSet8'], $row['UsrWghtSet9'], $row['UsrWghtSet10']);
+     $i++;
 } 
 
 $maxSets = max($UsrSetsQTY) + 1;
@@ -114,16 +114,19 @@ if ($modify) {
      echo "<form method=\"POST\" action=\"updateworkout.php\">";
 		 echo "<table border=1>";
 		 echo "<tr><td><a href=\"$current_url\">view records</a></td></tr>\n";
+		 echo "<input name=\"num\" type=\"hidden\" value=\"$num\">";
 		 for ($i=0; $i<$num; $i++) {
           if ($i == 0 || $MuscleGrpName[$i] != $MuscleGrpName[$sameGrp]) {
 					     echo "<tr><td colspan=$maxSets>$MuscleGrpName[$i]</td></tr>\n";
           } 
           echo "<tr><td colspan=$maxSets>$UsrExerName[$i]</td></tr>";
 		      echo "<tr><td>weight</td>";
- 					
+					echo "<input name=\"UsrWghtID[$i]\" type=\"hidden\" value=\"$UsrWghtID[$i]\">";
+					echo "<input name=\"UsrSetsQTY[$i]\" type=\"hidden\" value=\"$UsrSetsQTY[$i]\">";
+					
 					for ($m=0; $m<$UsrSetsQTY[$i]; $m++) {  //This loop is incorrect it will overwrite the array everytime the first loop comes around.
                echo "<td>";
-					     echo "<input name=\"UsrWghtSetEntry[$m]\" type=\"text\" size=\"3\" value=\"".$UsrWghtSet[$i][$m]."\"></td>\n";
+					     echo "<input name=\"UsrWghtSetEntry[$i][$m]\" type=\"text\" size=\"3\" value=\"".$UsrWghtSet[$i][$m]."\"></td>\n";
      			     }
 		      echo "</tr>";
           }
