7e439d0214fec3f3b2628f2888800fb403f98f29
chris4136 <chris4136@ca06981c-f144-0410-9d5b-9b1bebc65a9f>
2008-02-01 20:38:00 +0000

    initial checkin.  Update workout is pretty much finished, it takes all parameters and updates the database.

---
+++ updateworkout.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once "db.mysql.php";					 //Main MySQL access Class
+
+## Capture the number of recurds we have to update.
+$num = $_POST['num'];
+
+## Capture the number of sets associated with each exercise in the workout.
+for ($i=0; $i<$num; $i++) {
+$UsrSetsQTY[$i] = $_POST['UsrSetsQTY'][$i];
+}
+
+## Capture the Key of the record we want to update
+for ($i=0; $i<$num; $i++) {
+$UsrWghtID[$i] = $_POST['UsrWghtID'][$i];
+}
+
+## Capture our actual set values for updating the database.
+for ($i=0; $i<$num; $i++) {
+
+     for ($m=0; $m<$UsrSetsQTY[$i]; $m++) {
+          $UsrWghtSetEntry[$i][$m] = $_POST['UsrWghtSetEntry'][$i][$m];
+          echo "value = ".$UsrWghtSetEntry[$i][$m]."<br>";
+     }
+
+}
+
+$updateWorkout = new database_mysql();
+$updateWorkout->connect();
+
+for ($i=0; $i<$num; $i++) {
+
+     for ($m=0; $m<$UsrSetsQTY[$i]; $m++) {
+		 		 $sql = "UPDATE `wolf`.`usrweighttbl` SET `UsrWghtSet1` = ".$UsrWghtSetEntry[$i][0].", `UsrWghtSet2` = '".$UsrWghtSetEntry[$i][1]."', `UsrWghtSet3` = '".$UsrWghtSetEntry[$i][2]."', `UsrWghtSet4` = '".$UsrWghtSetEntry[$i][3]."', `UsrWghtSet5` = '".$UsrWghtSetEntry[$i][4]."', `UsrWghtSet5` = '".$UsrWghtSetEntry[$i][4]."', `UsrWghtSet6` = '".$UsrWghtSetEntry[$i][5]."', `UsrWghtSet7` = '".$UsrWghtSetEntry[$i][6]."', `UsrWghtSet8` = '".$UsrWghtSetEntry[$i][7]."', `UsrWghtSet9` = '".$UsrWghtSetEntry[$i][8]."', `UsrWghtSet10` = '".$UsrWghtSetEntry[$i][9]."' WHERE `usrweighttbl`.`UsrWghtID` = ".$UsrWghtID[$i]." LIMIT 1;";
+				 $updateWorkout->query($sql);
+				 }
+}
+//
+?>
+
+
